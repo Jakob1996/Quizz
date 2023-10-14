46d2c211d84a95a5c9491ec121aaaac3e7de06ef
Jakob1996 <jakub.miftadinow@gmail.com>
2023-10-14 21:09:39 +0200

    Add changes

--- app/src/main/java/com/example/quizz/activities/main/MainActivity.kt
+++ app/src/main/java/com/example/quizz/activities/main/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val vanController = rememberNavController()
+                    val nanController = rememberNavController()
                     NavigationComponent
                     StartQuizScreen()
                 }
